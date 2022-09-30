a1678bb246742db8e65ea25c5e2de46dcdf360d5
PascoPrzyborowski <unity01@gmx.net>
2022-09-30 12:05:46 +0200

    works

---
+++ works.py
@@ -0,0 +1,117 @@
+
+
+# transformer = ['a', 'u', 't', 'o', 'b', 'o', 't', 's']
+
+# print(transformer)
+# # str1 = ""
+# # str1.join(['a', 'u', 't', 'o', 'b', 'o', 't', 's'])
+# print("".join(transformer))
+# # print(str1)
+
+
+# lists = [["John",["Mary"], "Amy"],["32","43","51"]]
+# print(lists[0][1][0])
+
+# lists = [["John",[{"name":"Mary"}], "Amy"],["32","43","51"]]
+# print(lists[0][1][0]["name"])
+
+# seq = range(1,5,2)
+# print(seq)
+# print(list(seq)) 
+# s = range(1,5)
+# list(s)
+# print(s)
+# print(list(s))
+
+
+# sizes = ("s","m","l","xl","10")
+
+# for i in range(len(sizes)):
+#     print(i)sizes
+
+# for s in range(sizes):
+#     print(s)
+
+# sizes = ["s","m","l","xl","10"]
+# print(sizes)
+# print(sizes.index('m'))
+# print(sizes.count('m'))
+# #print(sizes.append('0','p'))
+
+# alpha = ['a','b','c','d']
+# print(alpha)
+# print(alpha[::-1])
+# print(alpha[::-2])
+# print(alpha[::2])
+# alpha.reverse()
+# print(alpha)
+
+# num = [4,3,2,100,0]
+# num.sort()
+# print(num)
+
+#
+# words = ['cat', 'aadvark', 'elephant', 'squirrel', 'hippo']
+# words.sort()
+# print(words)
+# # words = words.sort()
+# # print(words)
+# words.sort(reverse=True)
+# print(words)
+
+# words[0] = "rat"
+# words[0:2]= "lion"
+# print(words)
+# words[0:2]= ["lion"]
+# print(words)
+# words.remove('o')
+# words.remove('n')
+# print(words)
+
+# words = ['cat', 'aadvark', 'elephant', 'squirrel', 'hippo']
+# print(words)
+# words[1]= "Lion"
+# words[3]= "Lion"
+# print(words)
+
+# words = ['cat', 'aadvark', 'elephant', 'squirrel', 'hippo']
+# print(words)
+# words[1:3]= ["Lion"]
+# print(words)
+# words[0]=None
+# print(words)
+# words.append('last')
+# print(words)
+# list_a=[1,2,3]
+# list_b=[4,5,6]
+# print(list_a)
+# print(list_b)
+# print(list_a + list_b)
+
+# list_a=[1,2,3]
+# list_b=[4,5,6]
+# list_a.extend(list_b)
+# print(list_a)
+
+# alpha = ['a','b','c']
+# print(alpha)
+# alpha.insert(1, 'x')
+# print(alpha)
+# alpha.pop()
+# print(alpha)
+# alpha.pop(1)
+# print(alpha)
+# #help(alpha.pop)
+
+# alpha = ['a','b','c']
+# print(alpha)
+# alpha.remove('c')
+# print(alpha)
+
+# a = [1,2,3,]
+# b = [1,2,3,]
+# c = [3,2,1,]
+
+# print(a == b)
+# print(a == c)
+
